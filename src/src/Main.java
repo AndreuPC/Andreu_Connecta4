d6f652ba17ead783040cd9042590fd5d4ad641de
AndreuPC <Apuigserver16104@alumnes.iesmanacor.cat>
2023-05-15 10:06:37 +0200

    pintar introduint numeros

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,13 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-        int columna = 1;
-        int fila = 5;
-
         //tauler del joc
         int[][] tablero = new int[8][7];
-
-         tablero[fila][columna] = 4;
+        Scanner scanner = new Scanner(System.in);
 
         for (int columnas = 0; columnas < 7; columnas++) {
             for (int filas = 0; filas < 8; filas++) {
@@ -16,14 +13,34 @@ public class Main {
             System.out.println();
         }
 
+        // Introduir posicions
+        System.out.println("Introdueix les posicions (fila, columna) : ");
+        int filas, columnas;
+        while (true) {
+            filas = scanner.nextInt();
+            columnas = scanner.nextInt();
 
+            // Validar posicions
+            if (filas < 0 || filas >= 8 || columnas < 0 || columnas >= 7) {
+                System.out.println("Posició inválida. Intenta de nou amb una posició valida.");
+                continue;
+            }
 
+            // Asignar valor en la posición introducida
+            tablero[filas][columnas] = 1;
 
+            // Mostrar tauler actualizat
+            for (int a = 0; a < 7; a++) {
+                for (int b = 0; b < 8; b++) {
+                    System.out.print(tablero[b][a] + " ");
+                }
+                System.out.println();
+            }
+
+            System.out.println("Introdueix les posicions (fila, columna) : ");
+
+        }
 
     }
-
-
-
-
 
 }
