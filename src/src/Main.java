9b81a8dc9fcbadfd271bdf071d0fe02e0d101d1d
AndreuPC <Apuigserver16104@alumnes.iesmanacor.cat>
2023-05-15 09:46:49 +0200

    crear tauler

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        //tauler del joc
+
+        int[][] tablero = new int[8][7];
+
+        for (int columna = 0; columna < 7; columna++) {
+            for (int fila = 0; fila < 8; fila++) {
+                System.out.print(tablero[fila][columna] + " ");
+            }
+            System.out.println();
+        }
+
 
 
     }
