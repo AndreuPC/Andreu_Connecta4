471c9397e956b9c9d885fd73adaff7bd415e69bc
AndreuPC <Apuigserver16104@alumnes.iesmanacor.cat>
2023-05-15 09:52:00 +0200

    com pintar al tauler

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,15 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        //tauler del joc
 
+        int columna = 1;
+        int fila = 5;
+
+        //tauler del joc
         int[][] tablero = new int[8][7];
 
-        for (int columna = 0; columna < 7; columna++) {
-            for (int fila = 0; fila < 8; fila++) {
-                System.out.print(tablero[fila][columna] + " ");
+         tablero[fila][columna] = 4;
+
+        for (int columnas = 0; columnas < 7; columnas++) {
+            for (int filas = 0; filas < 8; filas++) {
+                System.out.print(tablero[filas][columnas] + " ");
             }
             System.out.println();
         }
+
+
 
 
 
